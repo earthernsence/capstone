438447273062d4edff22fe83d128514b2e383bab
Jace Royer <divine.icbm@gmail.com>
2023-11-03 10:08:00 -0500

    gsdfdf

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,4 +1,5 @@
 const wrapper = document.getElementById("wrapper");
+const slider = document.getElementById("slider");
 const gridEntry = () => {
     const cell = document.createElement("div");
     cell.className = "o-pixel";
@@ -8,19 +9,35 @@ const gridEntry = () => {
     return cell;
 }
 
-const grid = [];
+let grid = [];
 const GRID_DIMENSION = 500;
 
 function getDimensionsFromBrowser() {
     // use slider for squares on one size
     // calculate pixel size from 500x500 /
+    const val = parseInt(slider.value, 10);
+    return GRID_DIMENSION / val;
 }
 
-for (let i = 1; i <= 400; i++) {
-    const cell = gridEntry();
-    cell.id = `${i}`;
-    wrapper.append(cell);
-    grid[i] = cell;
+function getNeededNumberOfSquares() {
+    const SQUARE_PIXELS = Math.pow(GRID_DIMENSION, 2);
+    const SQUARE_PIXELS_OF_INDIVIDUAL_PIXEL = Math.pow(getDimensionsFromBrowser(), 2);
+    return SQUARE_PIXELS / SQUARE_PIXELS_OF_INDIVIDUAL_PIXEL;
+}
+
+function draw() {
+    for (let i = 1; i <= getNeededNumberOfSquares(); i++) {
+        const cell = gridEntry();
+        cell.id = `${i}`;
+        wrapper.append(cell);
+        grid[i] = cell;
+    }
+}
+
+slider.onchange = () => {
+    grid = [];
+    wrapper.children = [];
+    draw();
 }
 
 function hover(cell) {
