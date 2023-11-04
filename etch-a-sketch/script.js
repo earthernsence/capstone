7ab92aac40b9917c5e257e7f830fc8c38df0fb8c
earth <divine.icbm@gmail.com>
2023-11-03 20:23:19 -0500

    Finalise basic requirements for etch-a-sketch

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,11 +1,13 @@
 const wrapper = document.getElementById("wrapper");
 const slider = document.getElementById("slider");
+const eraser = document.getElementById("checkbox");
 const gridEntry = () => {
     const cell = document.createElement("div");
     cell.className = "o-pixel";
     cell.classList.add("o-pixel__empty");
-    const change = () => hover(cell);
-    cell.addEventListener("mouseover", change);
+    cell.addEventListener("mouseover", () => hover(cell));
+    cell.style.width = `${getDimensionsFromBrowser()}px`;
+    cell.style.height = `${getDimensionsFromBrowser()}px`;
     return cell;
 }
 
@@ -14,7 +16,7 @@ const GRID_DIMENSION = 500;
 
 function getDimensionsFromBrowser() {
     // use slider for squares on one size
-    // calculate pixel size from 500x500 /
+    // calculate pixel size from 500x500 grid which is predefined
     const val = parseInt(slider.value, 10);
     return GRID_DIMENSION / val;
 }
@@ -22,10 +24,14 @@ function getDimensionsFromBrowser() {
 function getNeededNumberOfSquares() {
     const SQUARE_PIXELS = Math.pow(GRID_DIMENSION, 2);
     const SQUARE_PIXELS_OF_INDIVIDUAL_PIXEL = Math.pow(getDimensionsFromBrowser(), 2);
-    return SQUARE_PIXELS / SQUARE_PIXELS_OF_INDIVIDUAL_PIXEL;
+    return Math.round(SQUARE_PIXELS / SQUARE_PIXELS_OF_INDIVIDUAL_PIXEL);
 }
 
 function draw() {
+    document.getElementById("dimensions-label").innerHTML = `Currently ${slider.value} x ${slider.value}`;
+    
+    wrapper.innerHTML = "";
+
     for (let i = 1; i <= getNeededNumberOfSquares(); i++) {
         const cell = gridEntry();
         cell.id = `${i}`;
@@ -34,13 +40,24 @@ function draw() {
     }
 }
 
+let useEraser = false;
+
 slider.onchange = () => {
     grid = [];
-    wrapper.children = [];
     draw();
+}
+
+function toggleEraser() {
+    useEraser = !useEraser;
+    document.getElementById("eraser-toggle").innerHTML = `Eraser: ${useEraser ? "ON" : "OFF"}`
+}
+
+function increment(up) {
+    up ? slider.value++ : slider.value--;
+    slider.onchange();
 }
 
 function hover(cell) {
     cell.className = "o-pixel";
-    cell.classList.add("o-pixel__filled");
+    useEraser ? cell.classList.add("o-pixel__empty") : cell.classList.add("o-pixel__filled");
 }
